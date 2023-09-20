255c644f11456cc911365f6e677dfe780a21549f
Aiswarya-keyvalue <82808431+Aiswarya-keyvalue@users.noreply.github.com>
2023-09-20 19:30:12 +0530

    amstrong number (#2)

--- amstrong.js
+++ amstrong.js
@@ -1,1 +1,13 @@
-console.log("merge conflict")
+var c = 0;
+for(i=1;i<=1000;i++){
+    var num = i;
+    c=0
+    while(num != 0){
+        var x = num % 10;
+        c += x*x*x
+        num = parseInt(num/10)
+    }
+    if (c == i) {
+        console.log(i)
+    }
+}
